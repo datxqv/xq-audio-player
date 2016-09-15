fb4a6255074ed52ce22900bb895b1c058273d70f
Le Thanh Dat <datxqvh@gmail.com>
2016-09-15 09:46:42 +0700

    Initial Commit

---
+++ AudioPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioPlayer
+//
+//  Created by PaditechDev1 on 9/15/16.
+//  Copyright © 2016 PaditechDev1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AudioPlayerTests/AudioPlayerTests.swift
@@ -0,0 +1,36 @@
+//
+//  AudioPlayerTests.swift
+//  AudioPlayerTests
+//
+//  Created by PaditechDev1 on 9/15/16.
+//  Copyright © 2016 PaditechDev1. All rights reserved.
+//
+
+import XCTest
+@testable import AudioPlayer
+
+class AudioPlayerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
